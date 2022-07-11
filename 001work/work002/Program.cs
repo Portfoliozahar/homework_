3852205485bcb0b4ab39727fd8e1178cd77b5c94
borodinets <assassqwas@gmail.com>
2022-07-11 22:41:40 +0500

    002

--- 001work/work002/Program.cs
+++ 001work/work002/Program.cs
@@ -1,2 +1,26 @@
 ﻿
-Console.WriteLine("Hello");
+
+
+        Console.WriteLine("Введи 2 числа и я скажу какое из них больше");
+
+        Console.WriteLine("Введи первое число ");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введи второе число ");
+        int b = int.Parse(Console.ReadLine());
+
+        bool n1 = a > b;
+        bool n2 = a == b;
+        bool n3 = a < b;
+
+        if (n1)
+            {
+                Console.WriteLine($" {a} = max");
+            }
+             else if (n2)
+            {
+                Console.WriteLine($"Числа {a} и {b} равны");
+            }
+             else if (n3)
+            {
+                Console.WriteLine($" {b} = max");
+            }
