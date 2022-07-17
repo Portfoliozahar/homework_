a971c6d858cd079cdc0d38e98226fc8db0870c66
borodinets <assassqwas@gmail.com>
2022-07-18 02:23:16 +0500

    n4

---
+++ 002work/work004/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("введите число  ");
+int a = int.Parse(Console.ReadLine());
+
+if (a < 1 || a > 7)
+{
+    Console.WriteLine("error");
+}
+else
+{
+    if (a < 6)
+    {
+        Console.WriteLine("нет");
+    }
+    else
+    {
+        Console.WriteLine("да");
+    }
+}
