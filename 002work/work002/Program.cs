548c90358f492d071fe9ddb196bee61093048665
borodinets <assassqwas@gmail.com>
2022-07-18 02:12:53 +0500

    n2

---
+++ 002work/work002/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+int DeleteSecondDigit(int Number)
+{
+    int FirstN = Number / 100;
+    int ThirdN = Number % 10;
+    return FirstN * 10 + ThirdN;
+}
+
+int a = new Random().Next(100,1000);
+Console.WriteLine(a);
+
+Console.WriteLine(DeleteSecondDigit(a));
