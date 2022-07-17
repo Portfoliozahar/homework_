f0e8f79af267469f06cb3d66931d8d4234f41290
borodinets <assassqwas@gmail.com>
2022-07-18 02:18:52 +0500

    n3

---
+++ 002work/work003/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного  числа или сообщает, что третьей цифры нет.
+void ThirdNumber (int a)
+{
+    if (a > -100 && a < 100){
+        Console.WriteLine("error");
+        return;
+    }
+    while(a > 1000 || a < -1000){
+        a = a / 10;
+    }
+    Console.WriteLine(a % 10);
+}
+Console.Write("Write a number  ");
+int a = int.Parse(Console.ReadLine());
+
+ThirdNumber(a);
