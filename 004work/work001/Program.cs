e2b6260d9e71a858ce89d954eba7dc6b9b7f6876
borodinets <assassqwas@gmail.com>
2022-08-04 21:19:04 +0500

    n1

---
+++ 004work/work001/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("введите степень числа: ");
+int b = int.Parse(Console.ReadLine());
+
+
+    int extent = a;
+    for(int i = 1; i<b; i++)
+    extent = extent * a;
+  
+
+
+Console.WriteLine (extent);
+
---
+++ 004work/work002/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+int sum = 0;
+while (a > 0) 
+{
+    sum += a % 10;
+    a /= 10;
+}
+
+Console.WriteLine (sum);
