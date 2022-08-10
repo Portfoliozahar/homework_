ee4cde8d7fa0c55f7e7b74e8104ae51c5f62b773
borodinets <assassqwas@gmail.com>
2022-08-11 00:56:23 +0500

    end

--- 005work/work2/Program.cs
+++ 005work/work2/Program.cs
@@ -1,1 +1,42 @@
-﻿1
+﻿void Print(int[] arr)
+
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] Nums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+int sum(int[] arr)
+{
+    int n = 0;
+
+    for (int i = 0; i < arr.Length; i+=2)
+    
+        
+        {
+            n += arr[i];
+
+        }
+    
+    return n;
+}
+
+int[] arr1= Nums(6,1,11);
+Print(arr1);
+Console.WriteLine(sum(arr1));
--- 005work/work3/Program.cs
+++ 005work/work3/Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿void Print(double[] arr)
+
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+double[] Nums(int size)
+
+{
+    double[] arr = new double[size];
+    Random n = new Random();
+
+    for (int i = 1; i < size; i++)
+    {
+        arr[i] = Math.Round(n.NextDouble() * (10 + 12) - 10, 2);
+
+    }
+    return arr;
+}
+
+void diff(double[] arr)
+{
+    double max = arr[0];
+    double min = arr[0];
+
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (max < arr[i])
+        {
+            max = arr[i];
+
+        }
+        else if (min > arr[i])
+        {
+            min = arr[i];
+        }
+    }
+    Console.WriteLine($"max: {max} and min {min}");
+    Console.WriteLine($"difference: {max}-{min})={Math.Round(max - min, 2)}");
+}
+
+double[]arr1= Nums(15);
+Print(arr1);
+diff(arr1);
