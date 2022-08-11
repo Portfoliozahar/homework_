42bfec0cb8b3998371cf2e5034986b57727c05f0
borodinets <assassqwas@gmail.com>
2022-08-12 01:18:03 +0500

    end

--- 006work/homework/1work/Program.cs
+++ 006work/homework/1work/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ // Напишите программу, которая будет создавать копию заданного массива(вещественные числа) с помощью поэлементного копирования.
+
+void Print(double[] arr)
+
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+double[] Nums(int size)
+
+{
+    double[] arr = new double[size];
+    Random n = new Random();
+
+    for (int i = 1; i < size; i++)
+    {
+        arr[i] = Math.Round(n.NextDouble() * (11 + 12) - 10, 2);
+
+    }
+    return arr;
+}
+
+double[] copy(double[] array)
+{
+    double[] new_arr = new double[array.Length];
+    for (int i = 0; i < new_arr.Length; i++)
+    {
+        new_arr[i] = array[i];
+    }
+    return new_arr;
+}
+
+double[] array = Nums (5);
+Print(array);
+Print(copy(array));
+
