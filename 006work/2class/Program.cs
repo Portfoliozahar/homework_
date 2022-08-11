e37e35434d8be35b0d35b80d25b91928bc8ac824
borodinets <assassqwas@gmail.com>
2022-08-12 00:22:50 +0500

    a

---
+++ 006work/2class/Program.cs
@@ -0,0 +1,13 @@
+﻿    string ChangeNum (int num)
+{
+    string result = "";
+    while (num>0)
+    {
+        result = num%2 + result;
+        num /= 2;
+    }
+    return result;
+}
+
+
+Console.WriteLine (ChangeNum(575));
